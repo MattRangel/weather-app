387ad7d39aa3fb810805e48d66ebe22f77123a4f
Matt Rangel <matt333three@aol.com>
2025-02-02 12:29:20 -0600

    Call getWeather on search form submit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./style.css";
-import "./weather.js";
+import getWeather from "./weather.js";
+
+document.querySelector("#weather-search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = e.target.location.value;
+  getWeather(location);
+});
