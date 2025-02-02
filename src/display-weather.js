c8717585370e9e6c1a9fa3c33982c270545b982d
Matt Rangel <matt333three@aol.com>
2025-02-02 15:19:39 -0600

    Implement function and styling necessary for visible and accessible loading

--- src/display-weather.js
+++ src/display-weather.js
@@ -1,9 +1,20 @@
-export default function displayWeather(data) {
-  const container = document.querySelector("#weather");
+const container = document.querySelector("#weather");
 
+export function displayWeather(data) {
   container.children.location.innerText = data.address;
   container.children.temperature.innerText = `${data.temp} °F`;
   container.children.conditions.innerText = data.conditions;
 
+  toggleWeatherLoading();
   container.classList = data.icon;
+}
+
+export function toggleWeatherLoading() {
+  container.classList.toggle("loading");
+  if (container.classList.contains("loading")) {
+    document
+      .querySelectorAll("#weather > *")
+      .forEach((elm) => (elm.innerText = ""));
+  }
+  container.ariaBusy = container.ariaBusy !== "true";
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./style.css";
 import getWeather from "./weather.js";
-import displayWeather from "./display-weather.js";
+import { displayWeather, toggleWeatherLoading } from "./display-weather.js";
 
 document.querySelector("#weather-search").addEventListener("submit", (e) => {
   e.preventDefault();
   const location = e.target.location.value;
   e.target.reset();
+  toggleWeatherLoading();
   getWeather(location).then(displayWeather);
 });
