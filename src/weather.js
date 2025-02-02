17c36d615103ac6adff0a53acb4a8bdfc6e1fe5f
Matt Rangel <matt333three@aol.com>
2025-02-02 11:32:49 -0600

    Implement function to get weather json data given a location

--- src/weather.js
+++ src/weather.js
@@ -1,1 +1,13 @@
-const apiKey = process.env.VISUAL_CROSSING_KEY;
+const KEY = process.env.VISUAL_CROSSING_KEY;
+
+export default async function getWeather(location) {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${KEY}&contentType=json`;
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    console.log(json);
+  } catch (error) {
+    console.error(error);
+  }
+}
