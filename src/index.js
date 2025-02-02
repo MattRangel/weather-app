2f588f71450d5c3b4507abf6f117bf2078ab1e0b
Matt Rangel <matt333three@aol.com>
2025-02-02 13:18:48 -0600

    Require search input to be filled. Reset search form after submit.

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import displayWeather from "./display-weather.js";
 document.querySelector("#weather-search").addEventListener("submit", (e) => {
   e.preventDefault();
   const location = e.target.location.value;
+  e.target.reset();
   getWeather(location).then(displayWeather);
 });
